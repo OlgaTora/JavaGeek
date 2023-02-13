0b5bee223845163ca90c06b8371c7fe6cde8d8d7
Olga Torres <bestlaly@mail.ru>
2023-02-13 17:00:54 +0300

    seminar6 hw add

--- src/main/java/Seminar6/home/Main.java
+++ src/main/java/Seminar6/home/Main.java
@@ -1,4 +1,7 @@
 package Seminar6.home;
+
+import java.util.List;
+
 /*
 Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
 Создать множество ноутбуков (ArrayList).
@@ -8,4 +11,17 @@ package Seminar6.home;
 
  */
 public class Main {
+    public static void main(String[] args) {
+        ClassNotebook notebook1 = new ClassNotebook("1", "Honor", "red", "8", "5");
+        ClassNotebook notebook2 = new ClassNotebook("2", "Honor", "white", "16", "25");
+        ClassNotebook notebook3 = new ClassNotebook("3", "Samsung", "red", "8", "15");
+        ClassNotebook notebook4 = new ClassNotebook("4", "Asus", "black", "4", "15");
+        ClassNotebook notebook5 = new ClassNotebook("5", "Honor", "blue", "16", "5");
+        List<ClassNotebook> listNotebooks = List.of(notebook1, notebook2, notebook3, notebook4, notebook5);
+
+
+        ActivitiesNotebook operation = new ActivitiesNotebook(listNotebooks);
+        operation.start();
+    }
 }
+
